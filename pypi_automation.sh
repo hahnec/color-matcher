72fdf0b9ce0cac716dc3ce58da455e7b22c0759c
hahnec <inbox@christopherhahne.de>
2020-02-28 00:54:27 +0100

    Merge remote-tracking branch 'origin/origin/master' into origin/master

--- pypi_automation.sh
+++ pypi_automation.sh
@@ -9,14 +9,6 @@ sudo rm -rf build dist
 # bundle pypi package
 sudo python3 setup.py sdist bdist_wheel
 
-# auto-update colour-matcher (cos of similar name)
-sed -i '' 's/color_matcher=/colour_matcher=/' setup.py
-sed -i '' "s/name='color_matcher'/name='colour_matcher'/" setup.py
-sudo python3 setup.py sdist bdist_wheel
-sed -i '' 's/colour_matcher=/color_matcher=/' setup.py
-sed -i '' "s/name='colour_matcher'/name='color_matcher'/" setup.py
-#for file in dist/color_matcher-*; do sudo cp -a $file "dist/colour_matcher-"$(echo $file | sed 's/.*matcher-//'); done
-
 # test upload and download
 python3 -m twine upload --repository testpypi dist/*
 python3 -m pip install --index-url https://test.pypi.org/simple/ color_matcher
@@ -24,3 +16,13 @@ python3 -m pip install --index-url https://test.pypi.org/simple/ color_matcher
 # production upload and download
 python3 -m twine upload dist/*
 python3 -m pip install color_matcher
+
+# auto-update colour-matcher (cos of similar name)
+sed -i '' 's/color_matcher=/colour_matcher=/' setup.py
+sed -i '' "s/name='color_matcher'/name='colour_matcher'/" setup.py
+sudo python3 setup.py sdist bdist_wheel
+sed -i '' 's/colour_matcher=/color_matcher=/' setup.py
+sed -i '' "s/name='colour_matcher'/name='color_matcher'/" setup.py
+#for file in dist/color_matcher-*; do sudo cp -a $file "dist/colour_matcher-"$(echo $file | sed 's/.*matcher-//'); done
+python3 -m twine upload --repository testpypi dist/*
+python3 -m twine upload dist/*
