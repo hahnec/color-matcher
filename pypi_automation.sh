7a18a135c542c41c1ad19212eca32eed92066ea4
hahnec <inbox@christopherhahne.de>
2020-02-28 00:20:15 +0100

    feat(pypi): occupy british typing "colour-matcher"n pypi

--- pypi_automation.sh
+++ pypi_automation.sh
@@ -8,7 +8,15 @@ sudo rm -rf build dist
 
 # bundle pypi package
 #python3 ../setup.py sdist
-python3 setup.py sdist bdist_wheel
+#python3 setup.py sdist bdist_wheel
+
+# occupy colour-matcher (cos of similar name)
+sed -i '' 's/color_matcher=/colour_matcher=/' setup.py
+sed -i '' "s/name='color_matcher'/name='colour_matcher'/" setup.py
+sudo python3 setup.py sdist bdist_wheel
+sed -i '' 's/colour_matcher=/color_matcher=/' setup.py
+sed -i '' "s/name='colour_matcher'/name='color_matcher'/" setup.py
+#for file in dist/color_matcher-*; do sudo cp -a $file "dist/colour_matcher-"$(echo $file | sed 's/.*matcher-//'); done
 
 # test upload and download
 python3 -m twine upload --repository testpypi dist/*
