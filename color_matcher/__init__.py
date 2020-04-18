a06a260c7edd13fa810978c6e2b79b75c8e66f13
hahnec <inbox@christopherhahne.de>
2020-04-18 20:23:11 +0200

    fix(kw): strip quote characters from argument strings

--- color_matcher/__init__.py
+++ color_matcher/__init__.py
@@ -16,7 +16,7 @@ Copyright (c) 2020 Christopher Hahne <inbox@christopherhahne.de>
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 from .top_level import ColorMatcher
 from .hist_matcher import HistogramMatcher
